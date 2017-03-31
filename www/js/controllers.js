1e5eec58b719c47b464001f5eb271c71740d38d6
abounie <adevinci@live.fr>
2017-03-31 14:27:09 +0200

    connection OK

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,9 +17,7 @@ angular.module('starter.controllers', [])
                 })
             }).success(function(result)
                 {
-                $scope.data = result;
-                //alert("data = " + $scope.data.reponse);
-                if ($scope.data.reponse == "oui")
+                if (result.reponse == "oui")
                     {
                     $ionicPopup.alert(
                         {
@@ -31,8 +29,7 @@ angular.module('starter.controllers', [])
                     {
                     $ionicPopup.alert(
                         {
-                        title:'Inscription',
-                            template:'Veuillez remplir tous les champs'
+                        title:'Mauvais identifiant/mot de passe !'
                         });
                     }
                 })
@@ -40,15 +37,14 @@ angular.module('starter.controllers', [])
                 {
                 $ionicPopup.alert(
                     {
-                        title:' Réseau !',
-                        template:'Problème d\'accès réseau !'
+                    title:'Problème d\'accès réseau !'
                     });
                 }
             );
         }
     })
 
-.controller('LoginCtrl', function($scope, $state, $http, $ionicPopup)
+.controller('LoginCtrl', function($scope, $state, $http, $ionicPopup, $ionicHistory)
     {
     $scope.login = function(user)
         {
@@ -63,14 +59,15 @@ angular.module('starter.controllers', [])
                 })
             }).success(function(result)
                 {
-                //$scope.data = result;
-                //alert("data = " + $scope.data.reponse);
-                if ($scope.data.reponse == "oui")
+                //alert("data = " + result.status + "<br/>" + $scope.userdata.user_id + "<br/>" + $scope.userdata.user_nom + "<br/>" + $scope.userdata.user_prenom);
+                if (result.status == "logged")
                     {
+                    sessionStorage.setItem('user_id', result.user_id);
+                    sessionStorage.setItem('user_nom', result.user_nom);
+                    sessionStorage.setItem('user_prenom', result.user_prenom);
                     $ionicPopup.alert(
                         {
-                            title:'Connexion',
-                            template:'Vous êtes connecté'
+                        title:'Correct !'
                         });
                     $state.go('tab.accueil');
                     }
@@ -78,8 +75,7 @@ angular.module('starter.controllers', [])
                     {
                     $ionicPopup.alert(
                         {
-                            title:'Connexion',
-                            template:'Mauvais identifiant/mot de passe !'
+                        title:'Mauvais identifiant/mot de passe !'
                         });
                     }
                 })
@@ -87,25 +83,52 @@ angular.module('starter.controllers', [])
                 {
                 $ionicPopup.alert(
                     {
-                    title:' Réseau !',
-                        template:'Problème d\'accès réseau !'
+                    title:'Problème d\'accès réseau !'
                     });
                 }
             );
         }
     })
 
+.controller('AccueilCtrl', function($scope, $state, $http, $ionicPopup, $ionicHistory)
+    {
+    $(document).ready(function()
+        {
+        //alert("id = " + sessionStorage.getItem('user_id'));
+        $http({
+            method: "post",
+            url: "http://upway-app.fr/app/dataload.php",
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: $.param(
+                {
+                id_user: sessionStorage.getItem('user_id')
+                })
+            }).success(function(result)
+                {
+                if (result.status == "logged")
+                    {
+                    //alert("logged");
+                    $scope.statistiques = result;
+                    sessionStorage.setItem('user_statistiques', $scope.statistiques);
+                    }
+                })
+            .error(function(error)
+                {
+                alert("erreur = " + error);
+                }
+            );
+        });
+    })
+
 .controller('FintrajetCtrl', function($scope, $state)
     {
-    $scope.$root.tabsHidden = "tabs-hide";
-
     $(document).ready(function()
         {
         $('.box-end-trajet').click(function()
             {
             $(this).toggleClass("box-checked");
             });
-
+        
         $('#ctp-manoeuvres-decrem').click(function()
             {
             if ($('#cpt-manoeuvres').text() > 0)
@@ -125,8 +148,6 @@ angular.module('starter.controllers', [])
     var map;
     var control;
 
-    $scope.$root.tabsHidden = "tabs-hide";
-
     function updateCarte()
         {
         var onSuccess = function(position)
@@ -138,20 +159,20 @@ angular.module('starter.controllers', [])
                 updateCarte();
                 }, 2000);
             };
-
+            
         navigator.geolocation.getCurrentPosition(onSuccess);
         }
-
+                      
     function InitialiserCarte()
         {
         map = L.map('map').setView(L.latLng(45.0441373, 3.8658617), 15);
         var tuileUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
         var osm = L.tileLayer(tuileUrl, {
-            minZoom: 8,
+            minZoom: 8, 
             maxZoom: 17
         });
         osm.addTo(map);
-
+            
         navigator.geolocation.getCurrentPosition(function(position)
             {
             control = L.Routing.control({
@@ -161,10 +182,10 @@ angular.module('starter.controllers', [])
                 showAlternatives: false
                 }).addTo(map);
             });
-
+            
         updateCarte();
         }
-
+    
     $(document).ready(function()
         {
         InitialiserCarte();
@@ -174,5 +195,4 @@ angular.module('starter.controllers', [])
 
 .controller('StaticCtrl', function($scope)
     {
-    $scope.$root.tabsHidden = "";
     })
